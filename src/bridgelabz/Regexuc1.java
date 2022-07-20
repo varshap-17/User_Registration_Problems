fa20b4c1d41e04162f3542111b2d32db371315e2
varsha p <varshap630@gmail.com>
2022-07-20 23:05:00 +0530

    Pattern matching first name

---
+++ src/bridgelabz/Regexuc1.java
@@ -0,0 +1,10 @@
+//As a User need to enter a valid First Name
+
+package bridgelabz;
+
+public class Regexuc1 {
+	public static void main(String[] args) {
+		String name="Name";
+		System.out.println(name.matches("^[A-Z a-z]*$"));
+	}
+}
