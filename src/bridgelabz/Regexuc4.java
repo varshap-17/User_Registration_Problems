2f3c79d619944332c7ae5a76fedd3ed947244f63
varsha p <varshap630@gmail.com>
2022-07-20 23:10:20 +0530

    Pattern matching Mobile format

---
+++ src/bridgelabz/Regexuc4.java
@@ -0,0 +1,10 @@
+//As a User need to follow pre -defined Mobile Format
+
+package bridgelabz;
+
+public class Regexuc4 {
+	public static void main(String[] args) {
+		String name="91 9184482235";
+		System.out.println(name.matches("^[0-9]*\s[0-9]*$"));
+	}
+}
